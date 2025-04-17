740f7ae858a6914fbd9c26d4b31f40f69f0b9b08
khadi <Kadirid9@gmail.com>
2025-04-17 02:29:16 -0700

    about page

--- src/app/components/About_Us/hero.tsx
+++ src/app/components/About_Us/hero.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
 import React from "react";
 
-export default function hero() {
+export default function Hero() { 
   return (
-    <section>
-      <div className="space-y-6 lg:mb-[96px]  mb-[49px]  pt-[49px] lg:pt-[129px]">
-        <p className="text-mygrey2 font-semibold text-[24px] leading-[100%] tracking-[0%] text-center ">
+    <section className="w-full">
+      <div className="space-y-6 lg:mb-[96px] mb-[49px] pt-[49px] lg:pt-[129px]">
+        <p className="text-mygrey2 font-semibold text-[24px] leading-[100%] tracking-[0%] text-center">
           About us
         </p>
         <p className="text-myblack font-bold lg:text-[96px] leading-[100%] tracking-[0%] text-center text-[48px]">
           Our Story
         </p>
       </div>
-      <div className="w-full justify-center flex">
+      <div className="w-full relative h-[600px]" >
         <Image
           src={"/images/imagehero2.svg"}
           alt={"credbevy"}
           priority={true}
-          width={1440}
-          height={600}
+          fill 
+          style={{ objectFit: "cover" }}  
+          quality={100}  
         />
       </div>
     </section>
